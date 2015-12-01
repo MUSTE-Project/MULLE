d81dbbc7db04f20b3ecd87a050bbe1821339c49e
Herbert Lange <sonofchaos@gmx.de>
2015-12-01 12:58:30 +0100

    Moved all code concerning trees to Tree module. muste.hs is only a test environment for these methods

--- Tree.hs
+++ Tree.hs
@@ -11,12 +11,15 @@ data UTree =
   UEApp UTree UTree Pos
   | UEFun CId Pos
 
-data GenericTree = Node String [GenericTree]
-
-data UGenericTree = UNode Pos String [UGenericTree]
 instance Show UTree where
   show (UEApp e1 e2 p) = "(App:" ++ show p ++ " " ++ show e1 ++ " " ++ show e2 ++ ")"
   show (UEFun cid p) = "(Fun:" ++ show p ++ " " ++ show cid ++ ")"
+  
+data GenericTree = Node String [GenericTree]
+
+data UGenericTree = UNode Pos String [UGenericTree]
+
+
   
 tree2utree :: Tree -> Int -> (Int,UTree)
 tree2utree (EApp e1 e2) i =
@@ -33,3 +36,28 @@ path2upath (UEApp e1 e2 pos) (p:rest)
   | p == 0 = let next = (path2upath e1 rest) in if isJust next then Just (pos:(fromJust next)) else Nothing
   | p == 1 = let next = (path2upath e2 rest) in if isJust next then Just (pos:(fromJust next)) else Nothing
   | otherwise = Nothing
+
+-- My version of showBracketedString with slightly different output and primarily used to understand the BracketedString data type
+showTree :: BracketedString -> String
+showTree (Leaf token) = "\"" ++ token ++ "\""
+showTree (Bracket cat fid lindex fun exps bss) =
+  "(" ++ showCId fun ++ ":" ++ showCId cat ++ " " ++ (unwords $ map showTree bss) ++ ")"
+
+-- Select a node in a BracketedString determined by a path
+selectNode :: BracketedString -> [Int] -> Maybe BracketedString
+selectNode t [] = Just t
+selectNode t (pos:rest) =
+  let branch = (selectBranch t pos) in
+  case branch of {
+    Just b ->  selectNode b rest ;
+    Nothing -> Nothing
+    }
+
+-- Given a BracketedString, select one of the possible branches determined by an integer
+selectBranch :: BracketedString -> Int -> Maybe BracketedString
+selectBranch (Leaf token) i
+  | i == 0 = Just (Leaf token)
+  | otherwise = Nothing
+selectBranch (Bracket cat fid lindex fun exps bss) i
+  | i >= length bss = Nothing
+  | otherwise = Just (bss !! i)
--- muste.hs
+++ muste.hs
@@ -1,35 +1,9 @@
 -- module Muste where
 import PGF
---import PGF.Internal
+
 import Data.Maybe
-
-type Path = [Int]
+import Tree
 type Weight = Int
-
--- My version of showBracketedString with slightly different output and primarily used to understand the BracketedString data type
-showTree :: BracketedString -> String
-showTree (Leaf token) = "\"" ++ token ++ "\""
-showTree (Bracket cat fid lindex fun exps bss) =
-  "(" ++ showCId fun ++ ":" ++ showCId cat ++ " " ++ (unwords $ map showTree bss) ++ ")"
-
--- Select a node in a BracketedString determined by a path
-selectNode :: BracketedString -> [Int] -> Maybe BracketedString
-selectNode t [] = Just t
-selectNode t (pos:rest) =
-  let branch = (selectBranch t pos) in
-  case branch of {
-    Just b ->  selectNode b rest ;
-    Nothing -> Nothing
-    }
-
--- Given a BracketedString, select one of the possible branches determined by an integer
-selectBranch :: BracketedString -> Int -> Maybe BracketedString
-selectBranch (Leaf token) i
-  | i == 0 = Just (Leaf token)
-  | otherwise = Nothing
-selectBranch (Bracket cat fid lindex fun exps bss) i
-  | i >= length bss = Nothing
-  | otherwise = Just (bss !! i)
 
 -- precompute :: PGF -> Tree -> Path -> [(Path, Tree,Weight)]
 -- precompute pgf tree [] = []
@@ -54,14 +28,10 @@ main =
     grammar <- readPGF "gf/ABCAbs.pgf"
     let lang = (head $ languages grammar)
     let scat = (startCat grammar)
---    let parsesB = parse_ grammar lang scat (Just 4) "a b c d e" -- "the wolf drinks the milk"
     let parses1 = parse grammar lang scat "b d e f" -- "the wolf drinks the milk"
-    let parses2 = parse grammar lang scat "d e a b c"
---    putStrLn $ showBracketedString (snd parsesB) -- map (\x -> showExpr [] x) parses
-    putStrLn $ showBracketedString $ head $ bracketedLinearize grammar lang (head parses1)
-    putStrLn $ showBracketedString $ head $ bracketedLinearize grammar lang (head parses2)
---    putStrLn $ show (mkApp (mkCId "bla") [(mkApp (mkCId "apply") [])])
-    putStrLn $ unwords $ map showTree $ concat (map (\p -> bracketedLinearize grammar lang p) parses1)
-    let node = selectNode (head $ bracketedLinearize grammar lang (head parses1)) [0,0,1,1,0]
-    putStrLn $ showBracketedString $ fromJust $ node -- map (\x -> showExpr [] x) parses
---    findPath (head $ bracketedLinearize grammar lang (head parses1)) node
+--    let foo = map (\p -> putStrLn $ show $ snd $ tree2utree p 0) parses1
+--    let bar = map (\p -> putStrLn $ show $ p) parses1
+    putStrLn $ show (head parses1)
+    putStrLn $ show $ snd $ tree2utree ( head parses1 ) 0
+    return ()
+    
