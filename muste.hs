ebd9e8af888b9b07be8df977afdfe352bf8f468f
Herbert Lange <sonofchaos@gmx.de>
2015-12-08 13:33:05 +0100

    various tests

--- muste.hs
+++ muste.hs
@@ -20,10 +20,23 @@ main =
     grammar <- readPGF "gf/ABCAbs.pgf"
     let lang = (head $ languages grammar)
     let scat = (startCat grammar)
-    let parses1 = parse grammar lang scat "b d e f" -- "the wolf drinks the milk"
+    let parses1 = parse grammar lang scat "b e d f" -- "the wolf drinks the milk"
 --    let foo = map (\p -> putStrLn $ show $ snd $ tree2utree p 0) parses1
 --    let bar = map (\p -> putStrLn $ show $ p) parses1
     putStrLn $ show (head parses1)
-    putStrLn $ show $ snd $ tree2utree ( head parses1 ) 0
+    let uparse = tree2utree ( head parses1 )
+    let gparse = tree2gtree ( head parses1 )
+--     let ugparse = gtree2ugtree gparse
+--     putStrLn $ show $ uparse
+-- --    putStrLn $ show $ fromJust $ path2upath uparse [0,1,0,0]
+--     putStrLn $ show $ gparse
+--     putStrLn $ show $ ugparse
+--     putStrLn $ show $ gtree2tree gparse
+--     putStrLn $ show $ gtree2cgtree gparse grammar
+--     putStrLn $ show $ cgsubtrees $ gtree2cgtree gparse grammar
+--    augment grammar $ gtree2cgtree gparse grammar
+    putStrLn $ show $ showfuntype $ getfuntype grammar (mkCId "g")
+    putStrLn $ show $ functionsByCat grammar (mkCId "A")
+    putStrLn $ show $ map (getfuntype grammar) (functionsByCat grammar (mkCId "A"))
     return ()
     
