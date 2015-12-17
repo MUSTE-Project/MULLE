28f1e57a384bb7b5683c83881eb2b91919607447
Herbert Lange <sonofchaos@gmx.de>
2015-12-17 16:47:48 +0100

    different tests - now grammar

--- muste.hs
+++ muste.hs
@@ -3,6 +3,7 @@ import PGF
 
 import Data.Maybe
 import Tree
+import Grammar
 type Weight = Int
 
 -- precompute :: PGF -> Tree -> Path -> [(Path, Tree,Weight)]
@@ -18,14 +19,14 @@ type Weight = Int
 main =
   do
     grammar <- readPGF "gf/ABCAbs.pgf"
-    let lang = (head $ languages grammar)
-    let scat = (startCat grammar)
-    let parses1 = parse grammar lang scat "b e d f" -- "the wolf drinks the milk"
+    -- let lang = (head $ languages grammar)
+    -- let scat = (startCat grammar)
+    -- let parses1 = parse grammar lang scat "b e d f" -- "the wolf drinks the milk"
 --    let foo = map (\p -> putStrLn $ show $ snd $ tree2utree p 0) parses1
 --    let bar = map (\p -> putStrLn $ show $ p) parses1
-    putStrLn $ show (head parses1)
-    let uparse = tree2utree ( head parses1 )
-    let gparse = tree2gtree ( head parses1 )
+    -- putStrLn $ show (head parses1)
+    -- let uparse = tree2utree ( head parses1 )
+    -- let gparse = tree2gtree ( head parses1 )
 --     let ugparse = gtree2ugtree gparse
 --     putStrLn $ show $ uparse
 -- --    putStrLn $ show $ fromJust $ path2upath uparse [0,1,0,0]
@@ -35,8 +36,9 @@ main =
 --     putStrLn $ show $ gtree2cgtree gparse grammar
 --     putStrLn $ show $ cgsubtrees $ gtree2cgtree gparse grammar
 --    augment grammar $ gtree2cgtree gparse grammar
-    putStrLn $ show $ showfuntype $ getfuntype grammar (mkCId "g")
-    putStrLn $ show $ functionsByCat grammar (mkCId "A")
-    putStrLn $ show $ map (getfuntype grammar) (functionsByCat grammar (mkCId "A"))
+    -- putStrLn $ show $ showfuntype $ getfuntype grammar (mkCId "g")
+    -- putStrLn $ show $ functionsByCat grammar (mkCId "A")
+    -- putStrLn $ show $ map (getfuntype grammar) (functionsByCat grammar (mkCId "A"))
+    putStrLn $ show $ pgf2grammar grammar
     return ()
     
