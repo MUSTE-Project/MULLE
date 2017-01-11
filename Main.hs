467129c1fb52cd39c81aeba3d9f51f3f966bdd50
Herbert Lange <sonofchaos@gmx.de>
2017-01-11 19:59:01 +0100

    hacked together a first working version

--- Main.hs
+++ Main.hs
@@ -1,5 +1,73 @@
-import qualified Muste as M
+import Muste
+import Muste.Tree
+import Muste.Grammar
+import PGF
+import Data.Set hiding (null)
+import Data.Maybe
+import System.Exit
+import Debug.Trace
 
+-- (Comment:9 (Item:6 this (Kind:5 pizza)) is (Quality:8 very (Quality:7 Italian)))
+-- Pred (This Pizza) (Very Italian)
+startTree = MetaTTree (read "{Pred:(Item->Quality->Comment) {This:(Kind->Item) {Pizza:Kind}} {Very:(Quality->Quality) {Italian:Quality}}}") empty --MetaTTree (TMeta wildCId) empty
+grammarFile = "gf/Foods.pgf"
+depth = 3 -- why???
+
+debug = False
+
+handleSelection :: MetaTTree -> Path -> [(String,MetaTTree)] -> Int -> IO (Maybe Click, MetaTTree)
+handleSelection tree path suggestions selection =
+  do
+    return (Nothing,MetaTTree (replaceNode (metaTree tree) path (metaTree $ snd $ suggestions !! ( selection - 1))) (subTrees tree))
+    
+handleClick :: Grammar -> Language -> MetaTTree -> [LinToken] -> Maybe Click -> Int -> IO (Maybe Click, MetaTTree)
+handleClick grammar language tree wordList click clickPos =
+  do
+    let newClick = fromJust $ updateClick click clickPos -- that should never be Nothing -> QuickCheck test for updateClick
+    -- Compute the path given the click position and click count
+    let tokenPos = pos newClick `div` 2
+    let path = if tokenPos == length wordList then [] else take (length (fst $ wordList !! tokenPos) - (count newClick - 1)) $ fst $ wordList !! tokenPos
+    putStrLn $ "You clicked on position " ++ show ( pos newClick ) ++ " for the " ++ show ( count newClick ) ++ " time. That gives token no. " ++ show tokenPos
+    putStrLn $ "The current path is " ++ show path
+    let extend = pos newClick `mod` 2 == 0 -- click between words
+    -- Get suggestions
+    let suggestions = getSuggestions grammar language tree path extend depth 
+    mapM_ (\(a,(b,_)) -> putStrLn $ show a ++ ". " ++ b) $ zip [1..] suggestions
+    -- do something to the tree
+    putStrLn "Which replacement to you want to have?"
+    input <- getLine
+    let selection = reads input :: ([(Int,String)])
+    (_,newTree) <- if (not $ null selection) && ((snd $ head selection) == "") then handleSelection tree path suggestions (fst $ head selection) else handleCommand tree click input 
+
+    return (Just newClick,newTree)
+
+handleCommand :: MetaTTree -> Maybe Click -> String -> IO (Maybe Click,MetaTTree)
+handleCommand tree click command
+  | command == "" = do return (click,tree)
+  | command == "quit" = exitSuccess 
+  | otherwise =
+    do
+      putStrLn "I don't know what you want from me"
+      return (click,tree)
+loop :: Grammar -> Language -> MetaTTree -> Maybe Click -> IO MetaTTree
+loop grammar language tree click =
+  do
+    -- Show the linearized tree
+    let wordList = linearizeTree grammar language tree
+    putStrLn $ linearizeList debug True wordList
+    -- Ask for the click
+    putStrLn "What position do you want to click on?"
+    input <- getLine
+    let selection = reads input :: [(Int,String)]
+    -- (newClick,newTree) <- if not $ null input && (snd $ head input) == "" then trace "1" $ handleClick grammar language tree wordList click (fst $ head input) else if null input then trace "2" $ handleCommand tree click "" else trace "3" $ handleCommand tree click (snd $ head input)
+    (newClick,newTree) <- if (not $ null selection) && ((snd $ head selection) == "") then handleClick grammar language tree wordList click (fst $ head selection) else handleCommand tree click input
+    loop grammar language newTree newClick
+  
 main =
   do
-    M.main
+    -- load the grammar
+    grammar <- pgfToGrammar <$> readPGF grammarFile
+    -- modify the tree, use the first language in the grammar. no previous click
+    loop grammar (head $ languages $ pgf grammar) startTree Nothing 
+    return ()
+  
--- Muste.hs
+++ Muste.hs
@@ -10,13 +10,28 @@ import PGF.Internal (Expr(..))
 import Debug.Trace
 import Data.Maybe
 import Data.List
+import Test.QuickCheck hiding (generate)
+import qualified Test.QuickCheck as Q
 
 type LinToken = (Path,String)
 
 -- | Type for a click that has both a position and a count
-type Count = Int
-type Click = (Pos,Count)
-    
+data Click = Click { pos :: Int, count :: Int } deriving (Show)
+
+-- | Click is in the Arbitrary class for QuickCheck
+instance Arbitrary Click where
+  arbitrary =
+    do
+      pos <- arbitrary
+      count <- arbitrary
+      return $ Click pos count
+      
+updateClick :: Maybe Click -> Pos -> Maybe Click
+updateClick Nothing pos = Just $ Click pos 1
+updateClick (Just (Click pos count)) newPos
+  | pos == newPos = Just $ Click pos (count + 1)
+  | otherwise = Just $ Click newPos 1
+  
 -- | The 'linearizeTree' function linearizes a MetaTTree to a list of tokens and pathes to the nodes that create it
 linearizeTree :: Grammar -> Language -> MetaTTree ->  [LinToken]
 linearizeTree grammar lang tree = 
@@ -28,7 +43,7 @@ linearizeTree grammar lang tree =
         deep :: LTree -> BracketedString -> [LinToken]
         deep _     (Bracket _ _   _ _ _ []) = []
         -- Ordinary leaf
-        deep ltree (Bracket _ fid _ _ _ [(Leaf token)]) =
+        deep ltree (Bracket _ fid _ _ _ [Leaf token]) =
           [(getPath ltree fid, token)]
         -- Meta leaf
         deep ltree (Bracket _ fid _ _ [n@(EMeta id)] _) =
@@ -40,7 +55,7 @@ linearizeTree grammar lang tree =
         -- End of node siblings
         broad _     _   []                 ts = ts
         -- Syncategorial word
-        broad ltree fid ((Leaf token):bss) ts =
+        broad ltree fid (Leaf token:bss) ts =
           let
             b = broad ltree fid bss ts
           in
@@ -64,64 +79,82 @@ linearizeTree grammar lang tree =
   in
     if not $ null brackets then bracketsToTuples ltree $ head brackets else [([],"?0")]
     
--- | The 'linearizeList' functions concatenates a token list to a string
-linearizeList :: Bool -> [LinToken] -> String
-linearizeList debug list =
-  if debug then unwords $ map (\(p,s) -> s ++ " " ++ show p) list
-  else unwords $ map (\(_,s) -> s) list
+-- | The 'linearizeList' functions concatenates a token list to a string, can contain the pathes for debugging and the positions
+linearizeList :: Bool -> Bool -> [LinToken] -> String
+linearizeList debug positions list =
+  let
+    conditionalString :: Bool -> String -> String
+    conditionalString True s = s
+    conditionalString False _ = ""
+    extendedList = zip [0..] $ concatMap (\e@(ep,es) -> [(ep," "),e]) list ++ [([]," ")]
+  in
+    --  if debug then unwords $ map (\(pos,(path,s)) -> "(" ++ show pos ++ ") " ++ s ++ " " ++ show path) extendedList
+    --else unwords $ map (\(pos,(_,s)) -> "(" ++ show pos ++ ") " ++ s) extendedList
+    unwords $ map (\(pos,(path,s)) -> conditionalString positions ("(" ++ show pos ++ ") ") ++ s ++ conditionalString debug (" " ++ show path)) extendedList
 
--- | The 'getNewTrees' function generates a set of related trees given a MetaTTree and a path
+-- | The 'getNewTrees' function generates a set of related subtrees given a MetaTTree and a path
 getNewTrees :: Grammar -> Language -> MetaTTree -> Path -> Int -> S.Set MetaTTree
 getNewTrees grammar lang tree path depth =
   let
-    clickPos = 0 -- TODO
-    tokens = [] -- TODO
-    -- Get Path from token list
-    (path,token) = tokens !! clickPos
-    -- Get Subtree at Path
     subTree :: TTree
     subTree = fromJust $ selectNode (metaTree tree) path
     -- Get category at path
     cat :: CId
     cat = getTreeCat subTree
     -- Generate Trees with same category
-    newTrees :: S.Set MetaTTree
-    newTrees = generate grammar cat depth
-    -- Filter trees
-    -- costList = S.map (match (MetaTTree subtree S.empty)) newTrees
-    -- TODO: Something with the costList
-    -- Idea: linearize subtree and each of newTrees and then order newTrees according to matching pre- and suffixes
-    subTreeLin :: [LinToken]
-    subTreeLin = linearizeTree grammar lang $ makeMeta subTree
-    -- newTreesLin :: [MetaTTree]
-    -- newTreesLin = doFilterMagic subTreeLin $ map (\t -> (t,linearizeTree grammar lang t)) $ S.toList newTrees
-    doFilterMagic :: [LinToken] -> [(MetaTTree,[LinToken])] -> [MetaTTree]
-    doFilterMagic subTree trees =
-      let
-        preAndSuffix :: [LinToken] -> [LinToken] -> Int
-        preAndSuffix [] _ = 0
-        preAndSuffix (a:resta) (b:restb) =
-          if a == b then 1 + preAndSuffix resta restb else preAndSuffix (reverse resta) (reverse restb)
-        magic :: [LinToken] -> (MetaTTree,[LinToken]) -> (MetaTTree,Int)
-        magic subTreeLin (tree,lin) = (tree,preAndSuffix subTreeLin lin)
-      in
-        map (\t -> fst $ magic subTree t ) $ sortBy (\(_,s1) -> \(_,s2) -> compare s1 s2 ) trees -- be more clever here
-        
+    newSubTrees :: S.Set MetaTTree
+    newSubTrees = generate grammar cat depth
   in
-    S.empty -- TODO
-
+    newSubTrees
+    
 -- | The 'treesToStrings' generates a list of strings based on the differences in similar trees
-treesToStrings :: Grammar -> Language -> S.Set MetaTTree -> S.Set String
+treesToStrings :: Grammar -> Language -> [MetaTTree] -> [String]
 treesToStrings grammar lang trees =
-  S.map (linearizeList False) $ S.map (linearizeTree grammar lang) trees
+  map (linearizeList False False . linearizeTree grammar lang) trees
 
--- | The 'getSuggestions' function generates a list of similar trees given a tree and a position in the token list
-getSuggestions :: Grammar -> Language -> [LinToken] -> MetaTTree -> Click -> Int -> S.Set (String, MetaTTree)
-getSuggestions grammar lang tokens tree click depth =
-  let
-    path = [] -- TODO something like take length - count from path at clickpos
-    nTrees = getNewTrees grammar lang tree path depth
-    suggestions = treesToStrings grammar lang $ nTrees
+-- Comput sum of the length of the longest common suffix and suffix for linearized trees
+preAndSuffix :: Eq a => [a] -> [a] -> (Int,Int)
+preAndSuffix [] _  = (0,0)
+preAndSuffix _  [] = (0,0)
+preAndSuffix a b =
+--  if a == b then 1 + preAndSuffix resta restb else preAndSuffix (reverse resta) (reverse restb)
+  let prefix :: Eq a => [a] -> [a] -> Int -> Int ->(Int,Int)
+      prefix [] _ pre suf = (pre,suf)
+      prefix _ [] pre suf = (pre,suf)
+      prefix (a:resta) (b:restb) pre suf
+        | a == b = prefix resta restb (pre + 1) suf
+        | otherwise = prefix resta restb pre (postfix (reverse (a:resta)) (reverse (b:restb)) 0)
+      postfix :: Eq a => [a] -> [a] -> Int -> Int
+      postfix [] _ suf = suf
+      postfix _ [] suf = suf
+      postfix (a:resta) (b:restb) suf
+        | a == b = postfix resta restb (suf + 1)
+        | otherwise = suf
   in
-    S.empty -- TODO something like zip suggestions (combineTrees tree nTrees)
+    prefix a b 0 0
+    
+createSortedTreeList :: Grammar -> Language ->MetaTTree -> S.Set MetaTTree -> [MetaTTree]
+createSortedTreeList grammar language tree trees =
+  let
+     treeList :: [MetaTTree]
+     treeList = S.toList trees
+     referenceLin :: [String]
+     referenceLin = map snd $ linearizeTree grammar language tree
+     costList :: [Int]
+     costList = map (\t -> uncurry (+) $ preAndSuffix referenceLin (map snd $ linearizeTree grammar language t)) treeList
+  in
+    sortBy (\a b -> compare (length $ linearizeTree grammar language a) (length $ linearizeTree grammar language b)) $ map snd $ sortBy (\(a,_) (b,_) -> compare b a ) $ zip costList treeList
+    
+                                                        
+  -- | The 'getSuggestions' function generates a list of similar subtrees given a tree and a position in the token list ordered by some measure
+getSuggestions :: Grammar -> Language -> MetaTTree -> Path -> Bool -> Int -> [(String, MetaTTree)]
+getSuggestions grammar language tree path extend depth =
+  let
+    extension = if extend then 1 else 0
+    subTree = flip MetaTTree S.empty $ fromJust $ selectNode (metaTree tree) path
+    nTrees = filter (\t -> ((length $ linearizeTree grammar language subTree) + extension) == (length $ linearizeTree grammar language t)) $ createSortedTreeList grammar language subTree $ S.filter (not . hasMetas . metaTree ) $ getNewTrees grammar language tree path depth
+    suggestions = treesToStrings grammar language nTrees
+  in
+    -- trace (show $ length nTrees) $ []
+    zip suggestions nTrees
 
