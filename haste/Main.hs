d0f6ce6a94a594cf624b92f842bdff32a00c58e0
Herbert Lange <sonofchaos@gmx.de>
2017-02-20 17:49:46 +0100

    changed demo to go more in the direction of the muste demo

--- haste/Main.hs
+++ haste/Main.hs
@@ -2,15 +2,25 @@ import Haste
 import Haste.Ajax
 import Haste.DOM
 import Haste.Concurrent
+
 import PGF
 import PGF.Internal
-import Data.String as S
+import Muste
+import Muste.Tree
+import Muste.Grammar
 
+import Data.String as S
 import Data.Binary(decode)
 import Data.ByteString(fromUArray)
 import Haste.Binary(getBlobData,toUArray)
-
 import Data.List
+import Data.Set hiding (null,map)
+
+-- (Comment:9 (Item:6 this (Kind:5 pizza)) is (Quality:8 very (Quality:7 Italian)))
+-- Pred (This Pizza) (Very Italian)
+startTree = MetaTTree (read "{Pred:(Item->Quality->Comment) {This:(Kind->Item) {Pizza:Kind}} {Very:(Quality->Quality) {Italian:Quality}}}") empty --MetaTTree (TMeta wildCId) empty
+grammarFile = "Foods.pgf"
+depth = 3 -- why???
 
 loadPGF :: String -> CIO PGF
 loadPGF url =
@@ -27,31 +37,44 @@ loadPGF url =
      writeLog (S.fromString ("Loaded "++ show (abstractName pgf))) :: CIO ()
      return pgf
 
-showLangs :: PGF -> IO ()
-showLangs pgf =
-  let
-    addListElem :: Elem -> String -> IO ()
-    addListElem parent text =
-      do
-        writeLog (S.fromString $ "added " ++ text)
-        child <- newElem "li"
-        newTextElem text >>= appendChild child
-        flip appendChild child parent
-        return ()
+musteMain :: PGF -> IO ()
+musteMain pgfGrammar =
+  do
+    let grammar = pgfToGrammar pgfGrammar
+    let language = head $ languages pgfGrammar
+    -- modify the tree, use the first language in the grammar. no previous click
+    drawTree grammar language startTree True
+    return ()
+
+drawTree :: Grammar -> Language -> MetaTTree -> Bool -> IO ()
+drawTree grammar language tree debug =
+  let drawWord :: LinToken -> Elem -> Bool -> IO ()
+      drawWord (p,s) elem debug =
+        do
+          wordSpan <- newElem "span" `with` [ attr "class" =: "word" ]
+          wordSpanText <- newTextElem
+          appendChild wordSpan wordSpanText
+          appendChild elem wordSpan
+          if debug then
+            (do
+                pathSpan <- newElem "span" `with` [ attr "class" =: "path" ]
+                pathSpanText <- newTextElem (show p)
+                appendChild pathSpan pathSpanText
+                appendChild elem pathSpan
+            )
+          else return ()
   in
     do
-      writeLog (S.fromString $ show $ map showCId $ languages pgf)
-      newList <- newElem "ul"
-      sequence_ $ map (addListElem newList) $ map showCId $ languages pgf
-      hl <- newElem "h1"
-      newTextElem "Languages" >>= appendChild hl
-      appendChild documentBody hl
-      appendChild documentBody newList
+  -- Show the linearized tree
+      let wordList = linearizeTree grammar language tree
+      writeLog (S.fromString $ show wordList) 
+      Just linTreeDiv <- elemById "linTree"
+      sequence_ $ map (\t -> drawWord t linTreeDiv debug) $ wordList
       return ()
-    
+
 main :: IO ()
 main =
   do
-    let pgf = loadPGF "Foods.pgf" :: CIO PGF
-    withResult pgf showLangs
+    let pgf = loadPGF grammarFile :: CIO PGF
+    withResult pgf musteMain
     return ()
