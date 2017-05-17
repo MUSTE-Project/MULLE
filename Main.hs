d282217e8d28ab476d40f4b6c2c3ec163613bbe1
Herbert Lange <sonofchaos@gmx.de>
2017-05-17 21:37:40 +0200

    changed some print statement to only run when debug enabled

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@ import Data.Set hiding (null,map)
 import Data.Maybe
 import System.Exit
 import Debug.Trace
+import Control.Monad
 
 startTree = (read "{Pred:(Item->Quality->Comment) {This:(Kind->Item) {Pizza:Kind}} {Very:(Quality->Quality) {Italian:Quality}}}")
 grammarFile = "gf/Foods.pgf"
@@ -25,17 +26,21 @@ handleClick debug grammar language tree wordList click clickPos =
     -- Compute the path given the click position and click count
     let tokenPos = trace (show wordList) $ pos newClick `div` 2
     let path = if tokenPos == length wordList then [] else take (length (fst $ wordList !! tokenPos) - (count newClick - 1)) $ fst $ wordList !! tokenPos
-    putStrLn $ "You clicked on position " ++ show ( pos newClick ) ++ " for the " ++ show ( count newClick ) ++ " time. That gives token no. " ++ show tokenPos
-    putStrLn $ "The current path is " ++ show path
+    putStrLn $ "You clicked on position " ++ show ( pos newClick ) ++ " for the " ++ show ( count newClick ) ++ " time."
+    when debug $ putStrLn $ "That gives token no. " ++ show tokenPos
+    when debug $ putStrLn $ "The current path is " ++ show path
     let extend = pos newClick `mod` 2 == 0 -- click between words
     -- Get suggestions
+    when debug $ putStrLn "Get new suggestions"
     let suggestions = getSuggestions grammar language tree path extend depth 
-    -- mapM_ (\(a,(b,_)) -> putStrLn $ show a ++ ". " ++ b) $ zip [1..] suggestions
+    when debug $ mapM_ (\(a,(b,_)) -> putStrLn $ show a ++ ". " ++ b) $ zip [1..] suggestions
+    when debug $ putStrLn "Linearize new suggestions"
     let linSubTree = map snd $ linearizeTree grammar language $ fromJust $ selectNode tree path
     mapM_ (\(a,(b,_)) -> putStrLn $ show a ++ ". " ++ printSuggestion linSubTree (words b)) $ zip [1..] suggestions
     -- do something to the tree
     let cat = getTreeCat $ fromJust $ selectNode tree path
-    putStrLn $ "The category is " ++ showCId cat ++ ". Which replacement do you want to have?"
+    when debug $ putStrLn $ "The category is " ++ cat ++ "."
+    putStrLn $ "Which replacement do you want to have?"
     putStrLn "Just press Enter to go back"
     input <- getLine
     let selection = reads input :: ([(Int,String)])
@@ -71,7 +76,6 @@ loop :: Grammar -> Language -> Bool -> TTree -> Maybe Click -> IO TTree
 loop grammar language debug tree click =
   do
     -- Show the linearized tree
-    putStrLn "Try to get the first tree linearized"
     let wordList = linearizeTree grammar language tree
     putStrLn $ linearizeList debug True wordList
     -- Ask for the click
@@ -79,17 +83,20 @@ loop grammar language debug tree click =
     putStrLn "Just press Enter to reset clicks"
     input <- getLine
     let selection = reads input :: [(Int,String)]
-    -- (newClick,newTree) <- if not $ null input && (snd $ head input) == "" then trace "1" $ handleClick grammar language tree wordList click (fst $ head input) else if null input then trace "2" $ handleCommand tree click "" else trace "3" $ handleCommand tree click (snd $ head input)
+    --(newClick,newTree) <- if not $ null input && (snd $ head input) == "" then trace "1" $ handleClick grammar language tree wordList click (fst $ head input) else if null input then trace "2" $ handleCommand tree click "" else trace "3" $ handleCommand tree click (snd $ head input)
     (newClick,newTree,newDebug) <- if (not $ null selection) && ((snd $ head selection) == "") then handleClick debug grammar language tree wordList click (fst $ head selection) else handleCommand debug tree Nothing input
     loop grammar language newDebug newTree newClick
+    return tree
   
 main =
   do
     -- load the grammar
-    putStrLn "Loading grammar"
+    when defaultDebug $ putStrLn "Loading grammar"
     grammar <- pgfToGrammar <$> readPGF grammarFile
-    putStrLn "Starting loop"
+    when defaultDebug $ putStrLn "Starting loop"
     -- modify the tree, use the first language in the grammar. no previous click
-    loop grammar (head $ languages $ pgf grammar) defaultDebug startTree Nothing 
-    return ()
+    let (v,p) = isValid startTree
+    if v then loop grammar (head $ tail $ languages $ pgf grammar) defaultDebug startTree Nothing
+    else error $ "Invalid starting tree. Problem in Node " ++ show p
+    return ()               
   
