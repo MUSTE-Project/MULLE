2d71d0009baefe46f4c1cf35bc62825e1d866acd
Herbert Lange <herbert.lange@cse.gu.se>
2017-08-31 17:09:45 +0200

    make a kind of complete standalone webserver to test the demo

--- ajax-backend/Main-Standalone.hs
+++ ajax-backend/Main-Standalone.hs
@@ -1,12 +1,55 @@
-module Main.Standalone where
+module Main where
 import Network.Shed.Httpd
+import Network.URI
+import Data.List
+import System.IO
 import Ajax
 
+filePath = "./demo"
+
+getFileName :: String -> String
+getFileName "/" = "muste.html"
+getFileName ('/':fn) = fn
+
+getType :: String -> String
+getType fn
+  | isSuffixOf "html" fn = "text/html"
+  | isSuffixOf "css" fn = "text/css"
+  | isSuffixOf "js" fn = "application/javascript"
+  | otherwise = "text/plain"
+
+getFileContent =
+  readFile 
 handleRequest :: Request -> IO Response
-handleRequest request =
-  do
-    let cm = decodeClientMessage (reqBody request)
-    return (Response 200 [("Content-type","text/json")] (show (cscore cm + 1))) 
+handleRequest request
+  | isPrefixOf "/cgi"(uriPath $ reqURI request) =
+      do
+        putStrLn $ "CGI" ++ (show request)
+        let cm = decodeClientMessage (reqBody request)
+        -- Get new Success
+        let nscore = cscore cm + 1
+        -- Check for Success
+        let ctreea = ctree $ ca cm
+        let ctreeb = ctree $ cb cm
+        let nsuccess = ctreea == ctreeb
+        -- Get new Suggestions
+        let na = ST (cgrammar $ ca cm) -- same language again
+                    (linearizeTree ctreea) -- linearization
+                    (generateMenus ctreea) -- menus
+        let nb = ST (cgrammar $ cb cm) -- same language again
+                    (linearizeTree ctreeb) -- linearization
+                    (generateMenus ctreeb) -- menus
+        -- New ServerMessage
+        let nsm = (SM nsuccess nscore na nb)
+        return (Response 200 [("Content-type","application/json")] (encodeServerMessage nsm))
+  | otherwise =
+      do
+        putStrLn $ "HTTP" ++ (show request)
+        let file = getFileName $ uriPath $ reqURI request
+        let typ = getType file
+        content <- getFileContent $ filePath ++ "/" ++ file
+        return (Response 200 [("Content-type",typ)] content)
+                
 main :: IO ()
 main =
   do
