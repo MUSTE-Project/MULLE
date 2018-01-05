516d1e475f9e637bd63804b1aa0a6aef74015043
Herbert Lange <herbert.lange@cse.gu.se>
2018-01-05 23:36:24 +0100

    main in Database.hs currently contains a small benchmark

--- ajax-backend/Database.hs
+++ ajax-backend/Database.hs
@@ -232,4 +232,8 @@ main =
   do
     putStrLn "Starting"
     con <- open "muste.db"
-    initDB con
+--     initDB con
+    (grammars,precs) <- initPrecomputed con
+    writeFile "/dev/null" (show precs)
+    putStrLn "Finished, shutting down"
+    close con
