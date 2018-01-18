145213fcb71ae4cccd13f4b34e92a08f4963be5c
Herbert Lange <herbert.lange@cse.gu.se>
2018-01-18 17:41:41 +0100

    move duplicate code in separate function

--- ajax-backend/Protocol.hs
+++ ajax-backend/Protocol.hs
@@ -41,21 +41,18 @@ handleClientRequest conn grammars prec body =
       do
         verified <- verifySession conn token
         (sourceLang,sourceTree,targetLang,targetTree) <- startLesson conn token lesson
-        let grammar = (grammars ! lesson)
-        let sourceTTree = gfAbsTreeToTTree grammar (read sourceTree :: Tree)
-        let targetTTree = gfAbsTreeToTTree grammar (read targetTree :: Tree)
-        let tempSourceLin = linearizeTree (grammar,read sourceLang :: Language) sourceTTree
-        let tempTargetLin = linearizeTree (grammar,read sourceLang :: Language) targetTTree
-        let sourceLin = map (\(path,lin) -> LinToken path lin (matched path sourceTTree targetTTree) tempSourceLin
-        let sourceMenu = Menu $ fromList $ map suggestionToCostTree $ suggestionFromPrecomputed (prec ! lesson ! (read sourceLang :: Language)) sourceTTree 
-        let targetLin = []
-        let targetMenu = Menu $ fromList $ map suggestionToCostTree $ suggestionFromPrecomputed (prec ! lesson ! (read targetLang :: Language)) targetTTree 
-        let a = ServerTree sourceLang sourceTree sourceLin sourceMenu
-        let b = ServerTree targetLang targetTree targetLin targetMenu
+        let (a,b) = assembleMenus lesson (sourceLang,sourceTree) (targetLang,targetTree)
         returnVerifiedMessage verified (SMMenuList lesson False 0 a b )
-        return "TODO"
-    handleMenuRequest token lesson score time a b =
-      return "TODO"
+    handleMenuRequest token lesson clicks time ctreea@(ClientTree langa treea) ctreeb@(ClientTree langb treeb)
+    -- Check if finished here
+      | treea == treeb =
+        do
+          return "TODO"
+      | otherwise =
+        do
+          verified <- verifySession conn token
+          let (a,b) = assembleMenus lesson (langa,treea) (langb,treeb)
+          returnVerifiedMessage verified (SMMenuList lesson False (clicks + 1) a b )
     -- either encode a message or create an error message dependent on the outcome of the verification of the session
     tryVerified :: (Bool,String) -> ServerMessage -> ServerMessage
     tryVerified (True,_) m = m
@@ -66,5 +63,21 @@ handleClientRequest conn grammars prec body =
     suggestionToCostTree (path,trees) =
       (path, [map (\(cost,lin,tree) -> CostTree cost (map (uncurry Linearization) lin) (show $ ttreeToGFAbsTree tree)) trees])
     -- Checks if a linearization token matches in both trees
-    matched p t1 t2 = if selectNode p t1 == selectNode p t2 then p else []
-
+    matched p t1 t2 = if selectNode t1 p == selectNode t2 p then p else []
+    -- gets the menus for a lesson, two trees and two languages
+    assembleMenus :: String -> (String,String) -> (String,String) -> (ServerTree,ServerTree)
+    assembleMenus lesson (sourceLang,sourceTree) (targetLang,targetTree) =
+      let grammar = (grammars ! lesson)
+          sourceTTree = gfAbsTreeToTTree grammar (read sourceTree :: Tree)
+          targetTTree = gfAbsTreeToTTree grammar (read targetTree :: Tree)
+          tempSourceLin = linearizeTree (grammar,read sourceLang :: Language) sourceTTree
+          tempTargetLin = linearizeTree (grammar,read sourceLang :: Language) targetTTree
+          sourceLin = map (\(path,lin) -> LinToken path lin (matched path sourceTTree targetTTree)) tempSourceLin
+          sourceMenu = Menu $ fromList $ map suggestionToCostTree $ suggestionFromPrecomputed (prec ! lesson ! (read sourceLang :: Language)) sourceTTree 
+          targetLin = map (\(path,lin) -> LinToken path lin (matched path sourceTTree targetTTree)) tempSourceLin
+          targetMenu = Menu $ fromList $ map suggestionToCostTree $ suggestionFromPrecomputed (prec ! lesson ! (read targetLang :: Language)) targetTTree 
+        -- At the moment the menu is not really a list of menus but instead a list with only one menu as the only element
+          a = ServerTree sourceLang sourceTree sourceLin sourceMenu
+          b = ServerTree targetLang targetTree targetLin targetMenu
+      in
+        (a,b)
