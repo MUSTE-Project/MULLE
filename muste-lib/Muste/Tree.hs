edc74ed4d8c9b2ed919536dd4577e686c2e2a9ad
Herbert Lange <herbert.lange@cse.gu.se>
2018-01-21 20:06:02 +0100

    simpler show function for ttrees (output look the same as showExpr [])

--- muste-lib/Muste/Tree.hs
+++ muste-lib/Muste/Tree.hs
@@ -1,6 +1,6 @@
 {- | This module provides implementations of different kinds of syntax trees and functions to manipulate them
 -}
-module Muste.Tree (TTree(..),LTree(..),Path,Pos,getPath,ttreeToLTree,ttreeToGFAbsTree,getPathes,maxDepth,getTreeCat,generateTrees,selectNode,replaceNode,gfAbsTreeToTTree,isValid) where
+module Muste.Tree (TTree(..),LTree(..),Path,Pos,getPath,ttreeToLTree,ttreeToGFAbsTree,getPathes,maxDepth,getTreeCat,generateTrees,selectNode,replaceNode,gfAbsTreeToTTree,isValid,showTTree) where
 
 import Muste.Tree.Internal
 import Muste.Feat
