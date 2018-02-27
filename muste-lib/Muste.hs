5a83471ec02d80b390ef6804cf50e793b3386ef6
Herbert Lange <herbert.lange@cse.gu.se>
2018-02-27 11:46:47 +0100

    moved functiond from protocol

--- muste-lib/Muste.hs
+++ muste-lib/Muste.hs
@@ -145,3 +145,23 @@ getScoredTrees context t p =
 getSuggestions :: Context -> TTree -> [(Path,[(Int,[(Path,String)],TTree)])]
 getSuggestions context tree =
   [ (p,getScoredTrees context tree p) | p <- getPathes tree]
+filterCostTrees :: [(Path,[[CostTree]])] -> [(Path,[[CostTree]])]
+filterCostTrees trees =
+  let
+    -- remove trees of cost 1
+    filtered1 = map (\(p,ts) -> (p,map (filter (\(CostTree c _ _) -> c /= 0)) ts)) trees :: [(Path,[[CostTree]])]
+    -- remove menu items that already appear further down the tree
+    filtered2 = thoroughlyClean $ sortBy (\(a,_) (b,_) -> compare (length b) (length a)) filtered1
+    -- remove empty menus
+    filtered3 = filter (\(p,ts) -> ts /= [] && ts /= [[]] ) filtered2 :: [(Path,[[CostTree]])]
+    -- sort by cost
+    sorted = map (\(p,ts) -> (p, map (sortBy (\(CostTree c1 _ _) (CostTree c2 _ _) -> compare c1 c2)) ts)) filtered3
+  in
+    sorted
+
+-- | Removes menu items further up in the tree if they already show up further down
+-- | Attention: Assumes that [[CostTree] actually contains only one menu
+thoroughlyClean :: [(Path,[[CostTree]])] -> [(Path,[[CostTree]])]
+thoroughlyClean [] = []
+thoroughlyClean ((p,[ts]):pts) =
+  (p,[ts]):(thoroughlyClean $ map (\(pp,[tt]) -> (pp,[tt \\ ts])) pts)
