719489c0350f56a6df332e76933595fa34bd50bb
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2018-07-23 11:57:10 +0200

    Move declarations upstream

--- muste-ajax/src/Database/Types.hs
+++ muste-ajax/src/Database/Types.hs
@@ -23,38 +23,12 @@ module Database.Types
 import Data.ByteString (ByteString)
 import Data.Text (Text)
 
-import Control.Exception (SomeException(..))
-import Control.Monad.Fail
 import Data.Time
-import Database.SQLite.Simple (SQLData(..))
-import Database.SQLite.Simple.Ok (Ok(..))
-import Database.SQLite.Simple.FromField (FromField(..), fieldData, ResultError(..), returnError)
-import Database.SQLite.Simple.ToField (ToField(..))
-import Text.Read
-import qualified Data.Text as T
 
 import Muste (TTree)
 
 type Blob = ByteString
 type Numeric = Integer
-
-parseNominalDiff :: Text -> Either String NominalDiffTime
-parseNominalDiff = fmap fromInteger . readEither . T.unpack
-
-instance MonadFail Ok where
-  fail = Errors . pure . SomeException . ConversionFailed mempty mempty
-
-instance FromField NominalDiffTime where
-  fromField fld = case fieldData fld of
-    (SQLText t) -> case parseNominalDiff t of
-      Right tm -> pure tm
-      Left e -> err ("couldn't parse UTCTime field: " ++ e)
-    _ -> err "expecting SQLText column type"
-    where
-    err = returnError ConversionFailed fld
-
-instance ToField NominalDiffTime where
-  toField = SQLText . T.pack . show
 
 type User =
   ( Text -- ^ @username@
