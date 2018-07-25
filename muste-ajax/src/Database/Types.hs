cf1f51f905e90073edd6541f93ce8b54f96286ab
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2018-07-25 13:00:52 +0200

    Soft documentation for the database types

--- muste-ajax/src/Database/Types.hs
+++ muste-ajax/src/Database/Types.hs
@@ -30,6 +30,12 @@ import Muste (TTree)
 type Blob = ByteString
 type Numeric = Integer
 
+-- | Representation of a 'User' in the database. Consists of:
+--
+-- * User name
+-- * Password
+-- * Salt
+-- * Is user enabled
 type User =
   ( Text -- @username@
   , Blob -- @password@
@@ -37,6 +43,12 @@ type User =
   , Bool -- @enabled@
   )
 
+-- | Representation of a 'Session' in the database. Consists of:
+--
+-- * User name
+-- * A token
+-- * Start time
+-- * End time
 type Session =
   ( Text -- @user@
   , Text -- @token@
@@ -45,6 +57,12 @@ type Session =
   )
 
 -- Probably should be @(Blob, Blob, Text, Numeric)@
+-- | Representation of an 'Exercise' in the database. Consists of:
+--
+-- * The source sentence
+-- * The target sentence
+-- * The lesson to which the exercise belongs
+-- * Timeout for the exercise
 type Exercise =
   ( Text -- @sourceTree@
   , Text -- @targetTree@
@@ -52,6 +70,16 @@ type Exercise =
   , Numeric -- @timeout@
   )
 
+-- | Representation of a 'Leson' in the database. Consists of:
+--
+-- * A name
+-- * A description
+-- * The grammar for the exercise
+-- * The source language
+-- * The target language
+-- * The number of exercises
+-- * Is it enabled
+-- * Is it repeatable
 type Lesson =
   ( Text -- @name@
   , Text -- @description@
@@ -63,6 +91,16 @@ type Lesson =
   , Bool -- @repeatable@
   )
 
+-- | Representation of a 'FinishedExercise' in the database. Consists
+-- of:
+--
+-- * The username of the one who finished it.
+-- * The source sentence
+-- * The target sentence
+-- * The name of the lesson it belongs to
+-- * The time it took to finish
+-- * The amount of clicks it took
+-- * The round it was in the lesson
 type FinishedExercise =
   ( Text -- @user@
   , Text -- @sourceTree@
@@ -73,12 +111,26 @@ type FinishedExercise =
   , Numeric -- @round@
   )
 
+-- | Representation of a 'StartedLesson' in the
+-- database. Consists of:
+--
+-- * The name of the lesson
+-- * The (name of the) user that started the lessson
+-- * The round
 type StartedLesson =
   ( Text -- @lesson@
   , Text -- @user@
   , Numeric -- @round@
   )
 
+-- | Representation of a 'FinishedLesson' in the
+-- database. Consists of:
+--
+-- * The name of the lesson
+-- * The (name of the) user that finished the exercise
+-- * The time it took to finish the exercise
+-- * The number of clicks it took to finish.
+-- * The number of rounds
 type FinishedLesson =
   ( Text -- @lesson@
   , Text -- @user@
@@ -87,6 +139,13 @@ type FinishedLesson =
   , Numeric -- @round@
   )
 
+-- | Representation of an 'ExerciseList' in the database. Consists of:
+--
+-- * User name
+-- * Source language
+-- * Target language
+-- * The lesson it belongs to
+-- * The round
 type ExerciseList =
   ( Text -- @user@
   , Text -- @sourceTree@
