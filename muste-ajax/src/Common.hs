f539517a03e8840f5e289820c485fb528c87283d
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2018-08-24 14:07:26 +0200

    Utility module

--- muste-ajax/src/Common.hs
+++ muste-ajax/src/Common.hs
@@ -1,17 +1,21 @@
-module Common (prettyShow, prettyTrace, prettyTraceId) where
+module Common (showPretty, tracePretty, tracePrettyId, traceShowId) where
 
 import Data.Text.Prettyprint.Doc (Pretty)
 import qualified Data.Text.Prettyprint.Doc as Doc
 
-import Debug.Trace
+import qualified Debug.Trace as Debug
 
-prettyShow ∷ Pretty a => a → String
-prettyShow = show . Doc.pretty
+{-# DEPRECATED traceShowId "Development aid remain in your code!!" #-}
+traceShowId ∷ Show a ⇒ a → a
+traceShowId = Debug.traceShowId
 
-{-# DEPRECATED prettyTrace "Development aid remain in your code!!" #-}
-prettyTrace ∷ Pretty a ⇒ a → b → b
-prettyTrace a = trace (prettyShow a)
+showPretty ∷ Pretty a => a → String
+showPretty = show . Doc.pretty
 
-{-# DEPRECATED prettyTraceId "Development aid remain in your code!!" #-}
-prettyTraceId ∷ Pretty a ⇒ a → a
-prettyTraceId a = trace (prettyShow a) a
+{-# DEPRECATED tracePretty "Development aid remain in your code!!" #-}
+tracePretty ∷ Pretty a ⇒ a → b → b
+tracePretty a = Debug.trace (showPretty a)
+
+{-# DEPRECATED tracePrettyId "Development aid remain in your code!!" #-}
+tracePrettyId ∷ Pretty a ⇒ a → a
+tracePrettyId a = Debug.trace (showPretty a) a
