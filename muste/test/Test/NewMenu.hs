62ce08687efb6267f61b265cfa47f21901ecd149
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2018-09-01 11:56:57 +0200

    Tried to simplify and improve Test.NewMenu

--- muste/test/Test/NewMenu.hs
+++ muste/test/Test/NewMenu.hs
@@ -9,6 +9,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Data.Set (Set)
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 import Control.Category ((>>>))
 import Control.Monad (when)
 import Control.Monad.Fail (MonadFail)
@@ -16,7 +17,7 @@ import Text.Printf
 import qualified Data.Containers as Mono
 import Data.Text.Prettyprint.Doc
   ( Pretty, Doc, pretty, nest
-  , vsep, (<+>), brackets, enclose
+  , vsep, (<+>), brackets, enclose, hsep
   )
 import Data.Text (Text)
 import Data.Function ((&))
@@ -69,118 +70,93 @@ tests = testGroup "NewFancyMenu" [menuLin, menuTrees]
 
 -- | A test-case consists of the following (in order of appearance):
 --
--- * A helpful name for the test-case.
 -- * The language that the two sentences are written in.
 -- * The sentence to get suggestions from.
--- * The "selection" to make.
--- * A sentence to (alt.: not) expect to be at this position in the
---   menu.
--- * Whether to expect the sentence to be amongst the suggestions, or
---   expect it *not* to be there.
-type LinTestCase = (String, Text, String, Menu.Selection, String, Bool)
+-- * A helpful name for the test-case.
+-- * The "selection" to make. If the selection is [] then the test is supposed to fail.
+-- * A sentence to (alt.: not) expect to be at this position in the menu.
+type LinTestCase = (Text, String, [(String, Menu.Selection, String)])
+
+linTestCases :: [LinTestCase]
+linTestCases = [ ("ExemplumSwe", "kungen älskar Paris",
+                  [ ("kungen->en kung", [(0,0)], "en kung älskar Paris")
+                  , ("kungen->Johan"  , [(0,1)], "Johan älskar Paris")
+                  , ("älskar->är"     , [(1,2)], "kungen är Paris")
+                  ])
+               , ("ExemplumSwe", "kungen är stor",
+                  [ ("kungen->huset"  , [(0,1)], "huset är stort")
+                  ])
+               , ("ExemplumSwe", "kungen är Paris",
+                  [ ("Paris->stor"    , [(2,3)], "kungen är stor")
+                  , ("Paris->en vän"  , [(2,3)], "kungen är en vän")
+                  ])
+               , ("ExemplumSwe", "det kalla huset är stort",
+                  [ ("DEL: det kalla" , [(0,2)], "huset är stort")
+                  ])
+               , ("ExemplumSwe", "huset är stort",
+                  [ ("INS: det kalla"  , [(0,0)], "det kalla huset är stort")
+                  ])
+               , ("ExemplumSwe", "Johan älskar Paris",
+                  [ ("Johan->en kung" , [(0,1)], "en kung älskar Paris")
+                  , ("NOT: Johan->en stor kung", [], "en stor kung älskar Paris")
+                  ])
+               , ("ExemplumSwe", "en kung älskar Paris",
+                  [ ("INS: stor"      , [(1,1)], "en stor kung älskar Paris")
+                  ])
+               , ("ExemplumSwe", "Johan älskar vännen",
+                  [ ("INS: idag"      , [(3,3)], "Johan älskar vännen idag")
+                  , ("INS: i Paris"   , [(3,3)], "Johan älskar vännen i Paris")
+                  ])
+               , ("ExemplumSwe", "Johan älskar vännen idag",
+                  [ ("DEL: idag"      , [(3,4)], "Johan älskar vännen")
+                  ])
+               , ("ExemplumSwe", "Johan älskar vännen i Paris",
+                  [ ("DEL: i Paris"   , [(3,5)], "Johan älskar vännen")
+                  ])
+               ]
+
 
 menuLin ∷ TestTree
-menuLin = testGroup "Linearization" $ mkTestLinearizations <$>
-  [ ("kungen->en kung", "ExemplumSwe", "kungen älskar Paris"        , [(0,0)], "en kung älskar Paris"       , expectSuccess)
-  , ("kungen->Johan"  , "ExemplumSwe", "kungen älskar Paris"        , [(0,1)], "Johan älskar Paris"         , expectSuccess)
-  , ("älskar->är"     , "ExemplumSwe", "kungen älskar Paris"        , [(1,2)], "kungen är Paris"            , expectSuccess)
-  , ("kungen->huset"  , "ExemplumSwe", "kungen är stor"             , [(0,1)], "huset är stort"             , expectSuccess)
-  , ("Paris->stor"    , "ExemplumSwe", "kungen är Paris"            , [(2,3)], "kungen är stor"             , expectSuccess)  -- FAIL
-  , ("Paris->en vän"  , "ExemplumSwe", "kungen är Paris"            , [(2,3)], "kungen är en vän"           , expectSuccess)
-  , ("DEL: det kalla" , "ExemplumSwe", "det kalla huset är stort"   , [(0,2)], "huset är stort"             , expectSuccess)
-  , ("INS: det kalla" , "ExemplumSwe", "huset är stort"             , [(0,0)], "det kalla huset är stort"   , expectSuccess)
-  , ("Johan->en kung" , "ExemplumSwe", "Johan älskar Paris"         , [(0,1)], "en kung älskar Paris"       , expectSuccess)
-  , ("INS: stor"      , "ExemplumSwe", "en kung älskar Paris"       , [(1,1)], "en stor kung älskar Paris"  , expectSuccess)
-  , ("Joh.->en st. k.", "ExemplumSwe", "Johan älskar Paris"         , [(0,1)], "en stor kung älskar Paris"  , expectFailure)
-  , ("INS: idag"      , "ExemplumSwe", "Johan älskar vännen"        , [(3,3)], "Johan älskar vännen idag"   , expectSuccess)  -- FAIL
-  , ("INS: i Paris"   , "ExemplumSwe", "Johan älskar vännen"        , [(3,3)], "Johan älskar vännen i Paris", expectSuccess)  -- FAIL
-  , ("DEL: idag"      , "ExemplumSwe", "Johan älskar vännen idag"   , [(3,4)], "Johan älskar vännen"        , expectSuccess)  -- FAIL
-  , ("DEL: i Paris"   , "ExemplumSwe", "Johan älskar vännen i Paris", [(3,5)], "Johan älskar vännen"        , expectSuccess)  -- FAIL
-  ]
-  where
-  expectSuccess = True
-  expectFailure = False
+menuLin = testGroup "Linearization" $ concatMap mkTestLinearizations linTestCases
 
 -- | Makes tests for menus based on 'Linearization's (as opposed to
 -- 'mkTest' that does it based on the internal syntax for sentences).
-mkTestLinearizations ∷ LinTestCase → TestTree
-mkTestLinearizations (nm, lang, src, sel, trg, isExpected)
-  -- Create a test-case with the given name.
-  = testCase nm $ do
-    -- Look up suggestions for source tree.  Convert results to their
-    -- string-representation.
-    sg ← Set.map Sentence.stringRep
-      <$> getSuggestions ctxt src sel
-    -- Get the string representation of the target.  We could just use
-    -- the argument passed in, but this also ensures that the grammar
-    -- understands the sentence.  Note that ambiguities may be
-    -- introduced here, so this returns a set.
-    let trgL = Set.map Sentence.stringRep
-          $ parseLin ctxt trg
-    -- Some tests are negative tests.  'expecter' reflects this.
-        expecter = if isExpected then id else not
-    -- This is where you need to "holde tungen like i munden".  We
-    -- test the interesection of the suggestions with the (alt.: not-)
-    -- expected result.  If the interesection is empty ('null') then
-    -- this is an error (alt.: a success).  However!  The following
-    -- statement will throw when the condition is *met*.
-    when (expecter $ Set.null (Set.intersection @String sg trgL))
-    -- Just some pretty-printing of the expected behaviour (shown on
-    -- failure).  'failDoc' is a helper that 'throw's and
-    -- pretty-prints some pretty-printable stuff in that case.
-      (failDoc $ nest 2 $ vsep
-        [ pretty @String $ "Testing: [" <> src <> "]  -->  [" <> trg <> "]"
-        , pretty @String " "
-        , pretty @String $ "Expected to " <> (if isExpected then "" else "*not* ") <> "find one of:"
-        , prettyTruncate limit trgL
-        , pretty @String " "
-        , pretty @String "Somewhere in the menu:"
-        , prettyTruncate limit sg
-        , pretty @String " "
-        ]
-      )
-    where
-    ctxt ∷ Context
-    ctxt = Util.unsafeGetContext grammar lang
-    limit = maxBound
+mkTestLinearizations ∷ LinTestCase → [TestTree]
+mkTestLinearizations (lang, src, tests) = map mkTestCase tests
+    where ctxt = Util.unsafeGetContext grammar lang
+          allMenuItems = getAllSuggestions ctxt src
+          mkTestCase (name, sel, trg)
+              = testCase name $
+                if null sel then
+                    when (not (null testSelections)) $
+                    testFailure [ "Expected to *not* find the target."
+                                , "But found it here:" <+> hsep (map Menu.prettySelection testSelections)
+                                ]
+                else if sel `elem` testSelections then
+                    when (length testSelections > 1) $
+                    testFailure [ "Did find the target in menu:" <+> Menu.prettySelection sel
+                                , "But also found it here:" <+> hsep (map Menu.prettySelection testSelections)
+                                ]
+                else 
+                    testFailure [ "Expected to find the target in menu:" <+> Menu.prettySelection sel
+                                , if null testSelections
+                                  then "But did not find it anywhere"
+                                  else "But found it here instead:" <+> hsep (map Menu.prettySelection testSelections)
+                                ]
+              where testSelections = [ sel' | (sel', item) <- allMenuItems, item == trg ]
+                    testFailure explanation = failDoc $ nest 2 $ vsep
+                                              [ pretty @String $ "Testing: [" <> src <> "]  -->  [" <> trg <> "]"
+                                              , vsep explanation
+                                              , " "
+                                              ]
 
-getSuggestions
-  ∷ MonadFail m
-  ⇒ Context
-  → String
-  → Menu.Selection
-  → m (Set (Linearization (Sentence.Token Unannotated)))
--- getSuggestions ctxt s sl = Set.fromList . map Menu.lin
--- getSuggestions ctxt s sl = Set.fromList . map (Sentence.linearization . undefined)
-getSuggestions ctxt s sl = Set.map snd
-  <$> Common.lookupFail err sl mn
-  where
-  mn ∷ NewFancyMenu
-  mn = Menu.getMenuItems ctxt s
-  err ∷ String
-  err = renderDoc
-    $ vsep
-      -- [ pretty @String (printf "Selection (%s) not found in menu for: \"%s\"" sl s)
-      [ pretty @String "Selection" <+> brackets (pretty sl) <+> pretty @String "not found in menu for:" <+> quotes (pretty @String s)
-      , pretty @String "Available selections:"
-      , pretty @[Menu.Selection] $ Mono.keys mn
-      ]
-  quotes = enclose (pretty @String "\"") (pretty @String "\"")
 
-parseLin ∷ Context → String → Set (Linearization (Token Annotated))
-parseLin ctxt
-  = parseTree'
-  >>> map (Sentence.linearization . mkL)
-  >>> Set.fromList
-  where
-  l = Linearization.ctxtLang ctxt
-  parseTree' ∷ String → [TTree]
-  parseTree' = Grammar.parseSentence grammar l
-  -- parseTree' = Grammar.parseSentence grammar (_ ctxt)
-  -- parseTree' = Sentence.disambiguate ctxt . fromString
-  mkL ∷ TTree → Annotated
-  -- mkL = Linearization.mkLinSimpl ctxt
-  mkL t = Sentence.annotated ctxt (error "Don't need the language here") t t t
+getAllSuggestions ∷ Context → String → [(Menu.Selection, String)]
+getAllSuggestions ctxt sent 
+    = [ (sel, Sentence.stringRep lin) |
+        (sel, items) <- Map.toList mmap,
+        (_sel, lin) <- Set.toList items ]
+    where mmap = Menu.unNewFancyMenu $ Menu.getMenuItems ctxt sent
 
 prettyTruncate ∷ Pretty a ⇒ Int → Set a → Doc b
 prettyTruncate n s = vsep $ [pretty trnc] ++ truncationWarning
