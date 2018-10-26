16075110f5a60616348ceb9051d1d0ce4da00cfb
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2018-10-26 11:39:23 +0200

    Split database into two modules

--- muste-ajax/src/Muste/Web/Database.hs
+++ muste-ajax/src/Muste/Web/Database.hs
@@ -38,9 +38,7 @@ import           Muste.Prelude
 import qualified Muste.Prelude.Unsafe      as Unsafe
 import           Muste.Prelude.Extra
 import           Muste.Prelude.SQL
-  ( Query, Connection, Only(Only), fromOnly
-  , FromRow, sql, ToNamed, NamedParam(..)
-  )
+  ( Query, Only(..), sql, NamedParam(..)  )
 import qualified Muste.Prelude.SQL         as SQL
 
 import qualified Data.List.NonEmpty        as NonEmpty
@@ -53,13 +51,10 @@ import           Data.ByteString (ByteString)
 import qualified Data.Text.Encoding        as T
 import qualified Data.Time.Clock           as Time
 import qualified Data.Time.Format          as Time
-import           Control.Monad.Reader
 
 -- FIXME QuickCheck seems like a heavy dependency just to get access
 -- to `shuffle`.
 import qualified Test.QuickCheck           as QC (shuffle, generate)
-
-import qualified Muste
 
 import qualified Muste.Web.Database.Types  as Types
 import qualified Muste.Web.Database.Types  as ActiveLessonForUser
@@ -69,42 +64,20 @@ import qualified Muste.Web.Database.Types  as ExerciseLesson
 import qualified Muste.Web.Database.Types  as User (User(..))
 import           Muste.Web.Types.Score (Score)
 
-data Error
-  = NoUserFound
-  | LangNotFound
-  | MultipleUsers
-  | NotCurrentSession
-  | SessionTimeout
-  | MultipleSessions
-  | NoExercisesInLesson
-  | NonUniqueLesson
-  | NotAuthenticated
-  | DriverError SomeException
-  | UserAlreadyExists
-  | NoActiveExercisesInLesson
-  | LessonAlreadySolved
-
-deriving stock instance Show Error
-instance Exception Error where
-  displayException = \case
-    NoUserFound         → "No user found."
-    LangNotFound        → "Could not find the languages."
-    MultipleUsers
-      →  "Well this is embarrasing.  "
-      <> "It would appear that someone managed "
-      <> "to steal youre identity."
-    NotCurrentSession   → "Not current session"
-    SessionTimeout      → "Session timeout"
-    MultipleSessions    → "More than one session"
-    NoExercisesInLesson → "No exercises for lesson"
-    NoActiveExercisesInLesson → "No unsolved exercises for lesson"
-    NonUniqueLesson     → "Non unique lesson"
-    NotAuthenticated    → "User is not authenticated"
-    DriverError e
-      →  "Unhandled driver error: "
-      <> displayException e
-    UserAlreadyExists → "The username is taken"
-    LessonAlreadySolved → "This lesson has already been solved.  You cannot restart it!"
+import           Muste.Web.Database.Class
+  ( MonadDB
+  , Error(..)
+  , executeNamed
+  , queryNamed
+  , DbT(..)
+  , Db
+  , HasConnection(..)
+  , getConnection
+  , MonadDatabaseError(..)
+  , runDbT
+  , query_
+  , executeManyNamed
+  )
 
 -- | @'hashPasswd' pw salt@ encodes @pw@ using PBKDF2 (using @salt@ as
 -- cyrptographic salt, doing 1e4 iterations and creating 1KiB
@@ -554,111 +527,6 @@ SELECT User
 FROM Session
 WHERE Token = :Token;
 |]
-
-class HasConnection v where
-  giveConnection ∷ v → Connection
-
-instance HasConnection Connection where
-  giveConnection = identity
-
-getConnection ∷ MonadDB r m ⇒ m Connection
-getConnection = giveConnection <$> ask
-
--- | Like 'MonadError' but only for 'Error's.
-class Monad m ⇒ MonadDatabaseError m where
-  throwDbError ∷ Error → m a
-  catchDbError ∷ m a → (Error → m a) → m a
-
-instance MonadDatabaseError IO where
-  throwDbError = throw
-  catchDbError = catch
-
-instance Monad m ⇒ MonadDatabaseError (ExceptT Error m) where
-  throwDbError = throwError @Error
-  catchDbError = catchError @Error
-
-instance Monad m ⇒ MonadDatabaseError (DbT m) where
-  throwDbError = DbT . throwError
-  catchDbError (DbT act) h = DbT $ catchError act (unDbT . h)
-
-type MonadDB r m =
-  ( HasConnection r
-  , MonadReader r m
-  , MonadIO m
-  , MonadDatabaseError m
-  )
-
-newtype DbT m a = DbT
-  { unDbT ∷ (ReaderT Connection (ExceptT Error m)) a
-  }
-
-deriving newtype instance Functor m ⇒ Functor     (DbT m)
-deriving newtype instance Monad m   ⇒ Applicative (DbT m)
-deriving newtype instance Monad m   ⇒ Monad       (DbT m)
-deriving newtype instance MonadIO m ⇒ MonadIO     (DbT m)
-deriving newtype instance Monad m   ⇒ MonadReader Connection (DbT m)
-deriving newtype instance Muste.MonadGrammar m ⇒ Muste.MonadGrammar (DbT m)
-
-instance MonadTrans DbT where
-  lift = DbT . lift . lift
-
-type Db a = DbT IO a
-
-runDbT ∷ DbT m a → Connection → m (Either Error a)
-runDbT (DbT db) c = runExceptT $ runReaderT db c
-
-query_
-  ∷ ∀ res r db . MonadDB r db
-  ⇒ FromRow res
-  ⇒ Query → db [res]
-query_ qry = wrappedWithCon $ \c →
-  SQL.query_ c qry
-
-queryNamed
-  ∷ ∀ res q r db
-  . MonadDB r db
-  ⇒ ToNamed q
-  ⇒ FromRow res
-  ⇒ Query
-  → q
-  → db [res]
-queryNamed q a = wrappedWithCon $ \c →
-  SQL.queryNamed c q (SQL.toNamed a)
-
-executeNamed
-  ∷ ∀ q db r
-  . MonadDB r db
-  ⇒ ToNamed q
-  ⇒ Query
-  → q
-  → db ()
-executeNamed q a = wrappedWithCon $ \c →
-  SQL.executeNamed c q (SQL.toNamed a)
-
--- This /may/ not be as efficient as using 'SQL.executeMany', but
--- unfortunately they do not exposed a version of that wih named
--- params.
-executeManyNamed
-  ∷ MonadDB r db
-  ⇒ ToNamed x
-  ⇒ Query
-  → [x]
-  → db ()
-executeManyNamed q xs = wrappedWithCon $ \c →
-  void $ traverse (SQL.executeNamed c q . SQL.toNamed) xs
-
-wrappedWithCon ∷ MonadDB r db ⇒ (Connection → IO a) -> db a
-wrappedWithCon act = do
-  c ← getConnection
-  wrapIoError $ act c
-
--- | Wraps any io error in our application specific 'DriverError'
--- wrapper.
-wrapIoError ∷ MonadDB r db ⇒ IO a → db a
-wrapIoError io =
-  liftIO (try io) >>= \case
-  Left e  → throwDbError $ DriverError e
-  Right a → pure a
 
 shuffle ∷ MonadIO m ⇒ [a] → m [a]
 shuffle = liftIO . QC.generate . QC.shuffle
