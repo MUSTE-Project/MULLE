185793acd380fa99cadd6fbd4018bfe91f3de107
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2019-01-27 11:16:20 +0100

    No need for installing grammars to a specific XDG data directory

--- muste/Setup.hs
+++ muste/Setup.hs
@@ -6,11 +6,6 @@ import qualified Distribution.Simple.Setup as Dist
 import Distribution.Types.HookedBuildInfo (HookedBuildInfo)
 import Distribution.Simple (UserHooks(..))
 import qualified Distribution.Simple as Dist
-import Turtle (Shell, Line)
-import qualified Turtle as Turtle
-import Data.Text (Text)
-import Control.Monad.IO.Class (MonadIO)
-import Control.Applicative (empty)
 
 main ∷ IO ()
 main
@@ -22,19 +17,4 @@ hooks = Dist.simpleUserHooks { preBuild }
   where
   preBuild ∷ Dist.Args → Dist.BuildFlags → IO HookedBuildInfo
   preBuild args flags
-    =  installGrammars
-    *> Dist.preBuild Dist.simpleUserHooks args flags
-
-installGrammars ∷ IO ()
-installGrammars = make ["-C", "data/grammars", "install"]
-
--- | @make@ a single target.  Throws on error.
-make ∷ MonadIO io ⇒ [Text] → io ()
-make = echoed "make"
-
-echoed ∷ MonadIO io ⇒ Text → [Text] → io ()
-echoed c xs = Turtle.stdout $ inprocs c xs empty
-
--- | Like 'Turtle.inproc' but throws on non-zero exit codes.
-inprocs ∷ Text → [Text] → Shell Line → Shell Line
-inprocs cmd args s = either id id <$> Turtle.inprocWithErr cmd args s
+    =  Dist.preBuild Dist.simpleUserHooks args flags
