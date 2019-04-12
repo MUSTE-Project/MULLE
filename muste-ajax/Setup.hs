c5adb1815a77d83e62446a55d8bb69b7385ec58c
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2019-04-12 16:06:18 +0200

    do not use Cabal custom-setup

--- muste-ajax/Setup.hs
+++ muste-ajax/Setup.hs
@@ -1,19 +1,4 @@
-{-# OPTIONS_GHC -Wall -Wcompat #-}
-{-# Language NamedFieldPuns, OverloadedStrings, TypeApplications #-}
 
-import qualified Distribution.Simple.Setup as Dist
-import Distribution.Types.HookedBuildInfo (HookedBuildInfo)
-import Distribution.Simple (UserHooks(..))
-import qualified Distribution.Simple as Dist
+import Distribution.Simple
 
-main :: IO ()
-main
-  = Dist.defaultMainWithHooks
-  $ hooks
-
-hooks :: UserHooks
-hooks = Dist.simpleUserHooks { preConf }
-  where
-  preConf :: Dist.Args -> Dist.ConfigFlags -> IO HookedBuildInfo
-  preConf args flags
-    = Dist.preConf Dist.simpleUserHooks args flags
+main = defaultMain
--- muste/Setup.hs
+++ muste/Setup.hs
@@ -1,19 +1,4 @@
-{-# OPTIONS_GHC -Wall -Wcompat #-}
-{-# Language NamedFieldPuns, OverloadedStrings, TypeApplications #-}
 
-import qualified Distribution.Simple.Setup as Dist
-import Distribution.Types.HookedBuildInfo (HookedBuildInfo)
-import Distribution.Simple (UserHooks(..))
-import qualified Distribution.Simple as Dist
+import Distribution.Simple
 
-main :: IO ()
-main
-  = Dist.defaultMainWithHooks
-  $ hooks
-
-hooks :: UserHooks
-hooks = Dist.simpleUserHooks { preBuild }
-  where
-  preBuild :: Dist.Args -> Dist.BuildFlags -> IO HookedBuildInfo
-  preBuild args flags
-    =  Dist.preBuild Dist.simpleUserHooks args flags
+main = defaultMain
