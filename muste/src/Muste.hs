5ef91ad6eede8d261619c9bdb0f7fe950581a627
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2019-05-09 15:44:06 +0200

    export muste functions that are used by the CLI

--- muste/src/Muste.hs
+++ muste/src/Muste.hs
@@ -3,7 +3,14 @@ module Muste
   ( module Muste.State
   , module Muste.Prune
   , module Muste.AdjunctionTrees
+  -- only used by muste-cli
+  , module Muste.Menu
+  , module Muste.Sentence
+  , module Muste.Selection
+  , module Muste.Util
   ) where
+
+-- used by muste-ajax
 
 import Muste.State
   ( MUState
@@ -12,8 +19,8 @@ import Muste.State
   , loadGrammarMU
   , getMenusMU
   , editDistanceMU
-  , LangLin
-  , LinMenus
+  , LangLin(..)
+  , LinMenus(..)
   )
 
 import Muste.Prune
@@ -23,4 +30,34 @@ import Muste.Prune
 
 import Muste.AdjunctionTrees
   ( BuilderInfo(BuilderInfo, searchDepth, searchSize)
+  )
+
+-- only used by muste-cli
+
+import Muste.State
+  ( lookupLessonMU
+  , parseSentenceMU
+  , getContextMU
+  )
+
+import Muste.Menu
+  ( Menu
+  , getMenuItems
+  )
+
+import Muste.Sentence
+  ( Context(Context, ctxtGrammar, ctxtLang)
+  , Linearization(Linearization)
+  , mergeL
+  , mkLinearization
+  , Token(Token) 
+  )
+
+import Muste.Selection
+  ( Selection
+  , runInterval
+  )
+
+import Muste.Util
+  ( putDocLn
   )
